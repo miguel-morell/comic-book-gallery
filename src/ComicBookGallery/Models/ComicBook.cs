64e0defed74b110054cfae37dea4f2d562dcdffd
Miguel Morell <miguel.morell@ruralsourcing.com>
2019-02-14 13:37:10 -0500

    Added Comic Book and Artist models

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,15 @@
+﻿namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTable { get; set; }
+        public int IssueNumber { get; set; }
+
+        public string DescriptionHtml { get; set; }
+
+        public Artist[] Artists { get; set; }
+
+        public bool Favorite { get; set; }
+    }
+}
